95335e33c3918adda47cf4d75975d829f74722e1
@Kirill Ushkov <ushkov.k@dbbest.com>
2014-02-13 14:48:10 +0200

    Initial Commit

---
+++ MultipleContactSelection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultipleContactSelection
+//
+//  Created by Kirill on 2/13/14.
+//  Copyright (c) 2014 Kirill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultipleContactSelection/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MultipleContactSelection
+//
+//  Created by Kirill on 2/13/14.
+//  Copyright (c) 2014 Kirill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
